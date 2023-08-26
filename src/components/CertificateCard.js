101be78bceba553e8b55b0d5dcfaeb5382785ac9
Prajwal C <109871236+prajwalc-22@users.noreply.github.com>
2023-08-27 00:09:59 +0530

    Update CertificateCard.js

--- src/components/CertificateCard.js
+++ src/components/CertificateCard.js
@@ -5,7 +5,7 @@ export const CertificateCard = ({ title, description, imgUrl, viewDemoLink, live
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="Project" />
+        <img src={imgUrl} alt="Certificates" loading="lazy" />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
